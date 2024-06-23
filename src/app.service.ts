8a768a27151b582efb4e7f700c4609cb94aaf059
visiosfera <ermano@gmail.com>
2024-06-23 11:00:41 -0300

    alteração

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'FUNCIONANDO NO JENKINS OUTRA VEZ';
+    return '0';
   }
 }
